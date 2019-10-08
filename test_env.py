fb743f4158784a7d3b0d849083b5c440e9d1ffe3
Maksim Sorokin <mksmsrkn@gmail.com>
2019-10-08 10:32:58 -0400

    env testinf script update

--- test_env.py
+++ test_env.py
@@ -4,15 +4,21 @@ from imageio import imwrite
 
 import envs
 
+def make_channel_last(image):
+    return np.rollaxis(image, 0, 3)
+
 if __name__ == '__main__':
     env = gym.make('Draw2D-v0',cuda=False)
     c = 0
-    for rollout_i in range(2):
+    for rollout_i in range(1):
         o, d = env.reset(), False
         while not d:
             c+=1
             a = np.random.rand(2)
             o,r,d,i = env.step(a)
             #d = i['is_success']
-        imwrite(f'test_imgs/{c}_i.png', o[0])
-        print(c, d)
+            i1,i2 = make_channel_last(o[0]), make_channel_last(o[1])
+            rend_img = np.concatenate([i1,i2],axis=1)
+            imwrite(f'test_imgs/{c}_i.png', rend_img)
+            print(c, d)
+            break
